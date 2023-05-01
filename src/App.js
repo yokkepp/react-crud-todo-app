44a91cfbcd17087d10355e226c69300719a06c02
yokkepp <yoseri430@gmail.com>
2023-05-02 05:51:30 +0900

    add local storage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const [todo, setTodo] = useState("");
+	const [todos, setTodos] = useState(() => {
+		const savedTodos = localStorage.getItem("todos");
+		if (savedTodos) {
+			return JSON.parse(savedTodos);
+		} else {
+			return [];
+		}
+	});
+
+	const handleInputChange = (e) => {
+		setTodo(e.target.value);
+	};
+
+	const handleFormSubmit = (e) => {
+		e.preventDefault();
+		if (todo !== "") {
+			const newTodos = [...todos, { id: todos.length + 1, title: todo.trim() }];
+			setTodos(newTodos);
+			setTodo("");
+			console.log(todos);
+		}
+	};
+
+	useEffect(() => {
+		localStorage.setItem("todos", JSON.stringify(todos));
+	}, [todos]);
+
+	return (
+		<div className='App'>
+			<form onSubmit={handleFormSubmit}>
+				<input
+					onChange={handleInputChange}
+					type='text'
+					placeholder='create a new todo!'
+					value={todo}
+				/>
+			</form>
+			<ul>
+				{todos.map((todo) => (
+					<li key={todo.id}>{todo.title}</li>
+				))}
+			</ul>
+		</div>
+	);
 }
-
-export default App;
